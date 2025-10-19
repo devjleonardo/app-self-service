5e7a2768f504e0d453a2e3e326a554a971c73513
devjleonardo <leonardo_cordeirobahia@hotmail.com>
2025-10-19 00:01:59 -0300

    chore: Adicionando plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,29 @@
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import prettier from "eslint-plugin-prettier";
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:prettier/recommended",
+  ),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      prettier,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "prettier/prettier": "warn",
+    },
+  },
 ];
-
-export default eslintConfig;
