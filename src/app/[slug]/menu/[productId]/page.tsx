674ef09a6b97ba275e7fadc90c5e87243506bb02
devjleonardo <leonardo_cordeirobahia@hotmail.com>
2025-10-19 13:02:06 -0300

    feat: Adicionando detalhes do produto

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -4,6 +4,7 @@ import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import ProductHeader from "./components/product-header";
+import ProductDetails from "./components/product-details";
 
 interface ProductPageProps {
   params: Promise<{ slug: string; productId: string }>;
@@ -12,16 +13,32 @@ interface ProductPageProps {
 const ProductPage = async ({ params }: ProductPageProps) => {
   const { slug, productId } = await params;
 
-  const product = await db.product.findUnique({ where: { id: productId } });
+  const product = await db.product.findUnique({
+    where: { id: productId },
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+          avatarImageUrl: true,
+          slug: true,
+        },
+      },
+    },
+  });
 
   if (!product) {
     notFound();
   }
 
+  if (product.restaurant.slug.toUpperCase() !== slug.toUpperCase()) {
+    notFound();
+  }
+
   return (
-    <>
+    <div className="flex h-full flex-col">
       <ProductHeader product={product} />
-    </>
+      <ProductDetails product={product} />
+    </div>
   );
 };
 
