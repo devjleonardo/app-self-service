f98e9552b650790e4d05416c1e674016a7816166
devjleonardo <leonardo_cordeirobahia@hotmail.com>
2025-10-21 00:29:11 -0300

    feat: Criando validação para que a quantidade de produtos no carrinho possa ser aumentada

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -15,7 +15,8 @@ interface CartProductItemProps {
 }
 
 const CartProductItem = ({ product }: CartProductItemProps) => {
-  const { decreaseProductQuantity } = useContext(CartContext);
+  const { decreaseProductQuantity, increaseProductQuantity } =
+    useContext(CartContext);
 
   return (
     <div className="flex items-center justify-between">
@@ -42,7 +43,11 @@ const CartProductItem = ({ product }: CartProductItemProps) => {
               <ChevronLeftIcon />
             </Button>
             <p className="w-7 text-sm">{product.quantity}</p>
-            <Button className="h-7 w-7 rounded-lg" variant="destructive">
+            <Button
+              className="h-7 w-7 rounded-lg"
+              variant="destructive"
+              onClick={() => increaseProductQuantity(product.id)}
+            >
               <ChevronRightIcon />
             </Button>
           </div>
