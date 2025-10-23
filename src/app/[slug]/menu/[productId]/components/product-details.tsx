7100896bda3a2a53b8404260b4789084a089e724
devjleonardo <leonardo_cordeirobahia@hotmail.com>
2025-10-23 01:52:30 -0300

    fix: Ajustando listagem de ingredientes

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -1,39 +1,44 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { formatCurrency } from "@/helpers/format-currency";
-import { Prisma, Product, Restaurant } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
 import { useContext, useState } from "react";
-import { CartContext } from "../../contexts/cart";
+
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { formatCurrency } from "@/helpers/format-currency";
+
 import CartSheet from "../../components/cart-sheet";
+import { CartContext } from "../../contexts/cart";
 
 interface ProductDetailsProps {
   product: Prisma.ProductGetPayload<{
-    include: { restaurant: { select: { name: true; avatarImageUrl: true } } };
+    include: {
+      restaurant: {
+        select: {
+          name: true;
+          avatarImageUrl: true;
+        };
+      };
+    };
   }>;
 }
 
 const ProductDetails = ({ product }: ProductDetailsProps) => {
   const { toggleCart, addProduct } = useContext(CartContext);
   const [quantity, setQuantity] = useState<number>(1);
-
   const handleDecreaseQuantity = () => {
     setQuantity((prev) => {
       if (prev === 1) {
         return 1;
       }
-
       return prev - 1;
     });
   };
-
   const handleIncreaseQuantity = () => {
     setQuantity((prev) => prev + 1);
   };
-
   const handleAddToCart = () => {
     addProduct({
       ...product,
@@ -41,7 +46,6 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
     });
     toggleCart();
   };
-
   return (
     <>
       <div className="relative z-50 mt-[-1.5rem] flex flex-auto flex-col overflow-hidden rounded-t-3xl p-5">
@@ -98,17 +102,15 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
 
             {/* INGREDIENTES */}
             <div className="mt-6 space-y-3">
-              <div className="flex items-center gap-1.5">
+              <div className="5 flex items-center gap-1">
                 <ChefHatIcon size={18} />
                 <h4 className="font-semibold">Ingredientes</h4>
               </div>
-              <p className="text-sm text-muted-foreground">
-                <ul className="list-disc px-5 text-sm text-muted-foreground">
-                  {product.ingredients.map((ingredient) => (
-                    <li key={ingredient}>{ingredient}</li>
-                  ))}
-                </ul>
-              </p>
+              <ul className="text-muted-fo list-disc px-5 text-sm text-muted-foreground">
+                {product.ingredients.map((ingredient) => (
+                  <li key={ingredient}>{ingredient}</li>
+                ))}
+              </ul>
             </div>
           </ScrollArea>
         </div>
