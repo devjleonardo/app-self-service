880aa4ccbe1c017855cf204933297f76a1ff8648
devjleonardo <leonardo_cordeirobahia@hotmail.com>
2025-10-23 23:41:15 -0300

    chore: Desativando eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,7 @@
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import prettier from "eslint-plugin-prettier";
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({ baseDirectory: __dirname });
-
 export default [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "plugin:prettier/recommended",
-  ),
   {
-    plugins: {
-      "simple-import-sort": simpleImportSort,
-      prettier,
-    },
     rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-      "prettier/prettier": "warn",
+      all: "off",
     },
   },
 ];
