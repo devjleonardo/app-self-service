5b584b06da4e376105249ffe420a15b0f842b5c7
devjleonardo <leonardo_cordeirobahia@hotmail.com>
2025-10-25 00:15:24 -0300

    feat: Ajustando botão de back do pedido

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Product } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 interface ProductHeaderProps {
   product: Pick<Product, "name" | "imageUrl">;
@@ -12,9 +12,14 @@ interface ProductHeaderProps {
 
 const ProductHeader = ({ product }: ProductHeaderProps) => {
   const router = useRouter();
+  const { slug } = useParams<{ slug: string }>();
 
   const handleBackClick = () => {
     router.back();
+  };
+
+  const handlerOrdersClick = () => {
+    router.push(`/${slug}/orders`);
   };
 
   return (
@@ -37,6 +42,7 @@ const ProductHeader = ({ product }: ProductHeaderProps) => {
         variant="secondary"
         size="icon"
         className="absolute right-4 top-4 z-50 rounded-full"
+        onClick={handlerOrdersClick}
       >
         <ScrollTextIcon />
       </Button>
--- src/app/[slug]/menu/components/header.tsx
+++ src/app/[slug]/menu/components/header.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Restaurant } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 interface RestauranteHeaderProps {
   restaurant: Pick<Restaurant, "name" | "coverImageUrl">;
@@ -12,9 +12,14 @@ interface RestauranteHeaderProps {
 
 const RestauranteHeader = ({ restaurant }: RestauranteHeaderProps) => {
   const router = useRouter();
+  const { slug } = useParams<{ slug: string }>();
 
   const handleBackClick = () => {
     router.back();
+  };
+
+  const handlerOrdersClick = () => {
+    router.push(`/${slug}/orders`);
   };
 
   return (
@@ -37,6 +42,7 @@ const RestauranteHeader = ({ restaurant }: RestauranteHeaderProps) => {
         variant="secondary"
         size="icon"
         className="absolute right-4 top-4 z-50 rounded-full"
+        onClick={handlerOrdersClick}
       >
         <ScrollTextIcon />
       </Button>
